28558c005a8fcbb4f6495280c4d5385c0910f484
Matija <vorkapicmatija@gmail.com>
2019-12-24 13:36:16 +0100

    First commit

---
+++ app/src/main/java/com/example/android/releasedatekt/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.android.releasedatekt.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.releasedatekt.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
