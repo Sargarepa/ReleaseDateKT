c6d07317becf664e7c88ccb8a1d9e8fb66a67c2a
Matija <vorkapicmatija@gmail.com>
2019-12-25 13:53:06 +0100

    Add database entity for Movie class and an extension function to turn a list of database Movie entries into a list of domain model Movies

---
+++ app/src/main/java/com/example/android/releasedatekt/database/DatabaseEntities.kt
@@ -0,0 +1,38 @@
+package com.example.android.releasedatekt.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.android.releasedatekt.domain.Movie
+import java.util.*
+
+@Entity
+data class DatabaseMovie constructor(
+    @PrimaryKey
+    val id: Int,
+    val popularity: Int,
+    val voteCount: Int,
+    val posterPath: String,
+    val language: String,
+    val title: String,
+    val genreIds: List<Int>,
+    val voteAverage: Float,
+    val overview: String,
+    val releaseDate: Date
+)
+
+fun List<DatabaseMovie>.asDomainModel(): List<Movie> {
+    return map {
+        Movie(
+            id = it.id,
+            popularity = it.popularity,
+            voteCount = it.voteCount,
+            posterPath = it.posterPath,
+            language = it.language,
+            title = it.title,
+            genreIds = it.genreIds,
+            voteAverage = it.voteAverage,
+            overview = it.overview,
+            releaseDate = it.releaseDate
+        )
+    }
+}
