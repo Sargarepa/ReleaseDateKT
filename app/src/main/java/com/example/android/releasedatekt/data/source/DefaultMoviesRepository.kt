cd22e46db98a71aaaffa280f7e1100f6a378b540
unknown <vorkapicmatija@gmail.com>
2020-06-10 19:15:11 +0200

    Remove old repository methods for retrieving movies

--- app/src/main/java/com/example/android/releasedatekt/data/source/DefaultMoviesRepository.kt
+++ app/src/main/java/com/example/android/releasedatekt/data/source/DefaultMoviesRepository.kt
@@ -26,7 +26,7 @@ class DefaultMoviesRepository
         else observeLocalPagedMovies()
 
 
-    fun observeLocalPagedMovies(): LiveData<PagedList<Movie>>? {
+    private fun observeLocalPagedMovies(): LiveData<PagedList<Movie>>? {
         val dataSourceFactory = movieDao.getPagedMoviesWithGenres().map {
             it.asDomainModelMovie()
         }
@@ -36,33 +36,13 @@ class DefaultMoviesRepository
         ).build()
     }
 
-    fun observeRemotePagedMovies(ioCoroutineScope: CoroutineScope): LiveData<PagedList<Movie>>? {
+    private fun observeRemotePagedMovies(ioCoroutineScope: CoroutineScope): LiveData<PagedList<Movie>>? {
         val dataSourceFactory = MoviePageDataSourceFactory(movieRemoteDataSource, movieDao, ioCoroutineScope)
         return LivePagedListBuilder(dataSourceFactory, MoviePageDataSourceFactory.pagedListConfig()).build()
     }
 
     //TODO
     fun observeMovie() = null
-
-    fun getMovies(scope: CoroutineScope): LiveData<PagedList<Movie>> {
-        val dataSourceFactory = movieDao.getPagedMoviesWithGenres().map {
-            it.asDomainModelMovie()
-        }
-
-        val boundaryCallback =
-            MovieBoundaryCallback(
-                scope,
-                ::refreshMovies
-            )
-
-
-        return LivePagedListBuilder(
-            dataSourceFactory,
-            DATABASE_PAGE_SIZE
-        )
-            .setBoundaryCallback(boundaryCallback)
-            .build()
-    }
 
     suspend fun refreshMovies(page: Int) {
         withContext(Dispatchers.IO) {
@@ -84,7 +64,4 @@ class DefaultMoviesRepository
         }
     }
 
-    companion object {
-        private const val DATABASE_PAGE_SIZE = 20
-    }
 }
